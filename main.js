dd20dd204bc3d85b5b9c216519bc8319e9808c7a
Livii Iabanzhi <liabanzh@e1r7p16.unit.ua>
2018-05-26 20:42:32 +0300

    Already not gavno code

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ const http = require('http')
 var fs = require('fs');
 var bodyParser = require('body-parser');
 var express = require('express');
+var async = require('async');
 var db = require('./db.js');
 
 var app = express();
@@ -14,12 +15,6 @@ function default_callback(err, res) {
 	} else {
 		console.log(res);
 	}
-}
-
-function errorCallback(error, errorCode, response) {
-	console.log(error);
-	response.json({error: errorCode});
-	response.end();
 }
 
 app.get('/', function(request, response) {
@@ -39,81 +34,159 @@ app.get('/requestScript.js', function(request, response) {
 	console.log("JS script send! 200");
 });
 
+// Add new operation and change balance
+
 app.post('/api/add/', function(request, response) {
 	var dataInCards = {contract_id: request.body.contract_id, balance: 0};
 	var dataInOperations = {contract_id: request.body.contract_id, bill: request.body.bill, type: request.body.type === true ? 'D' : 'W'};
 
-	if (dataInOperations.contract_id.length === 17 && dataInOperations.contract_id.startsWith("2625") && dataInOperations.bill !== 0) {
+	function getUserCard(callback) {
 		db.selectFromDBwithSelector("cards", dataInCards.contract_id, function(err, res) {
 			if (err) {
-				errorCallback(err, 3, response);
-				return ;
-			}
-			if (!res || res.length === 0) {
-				dataInCards.balance = dataInOperations.bill * (request.body.type ? 1 : -1);
-				db.insertInCards(dataInCards, function(err, res) {
-					if (err) response.json({error: 6});
-				});
+				callback(err, {error: 3});
 			} else {
-				dataInCards.balance = res[0].balance + (dataInOperations.bill * (request.body.type === true ? 1 : -1));
-				db.changeRowInDB("cards", dataInCards.balance, dataInCards.contract_id, function(err, res) {
-					if (err) response.json({error: 6});
-				});
+				callback(null, res);
 			}
 		});
+	}
+
+	function changeUserBalance(prevResult, callback) {
+		if (!prevResult || prevResult.length === 0) {
+			dataInCards.balance = dataInOperations.bill * (request.body.type ? 1 : -1);
+			db.insertInCards(dataInCards, function(err, res) {
+				if (err) {
+					callback(err, {error: 6});
+				} else {
+					callback(null);
+				}
+			});
+		} else {
+			dataInCards.balance = prevResult[0].balance + (dataInOperations.bill * (request.body.type === true ? 1 : -1));
+			db.insertInCards(dataInCards, function(err, res) {
+				if (err) {
+					callback(err, {error: 6});
+				} else {
+					callback(null);
+				}
+			});
+		}
+	}
+
+	function addNewOperation(callback) {
 		db.insertInOperations(dataInOperations, function (err, res) {
-			if (err) response.json({error: 5});
-			else {
-				db.selectFromDB("operations", function(error, result) {
-					if (error) response.json({error: 2});
-					else response.json(result);
-        			response.end();
-				});
+			if (err) {
+				callback(err, {error: 5});
+			} else {
+				callback(null);
 			}
 		});
-	} else {
-		response.json({error: 1});
-		response.end();
+	}
+
+	function getAllDataAndResponce(callback) {
+		db.selectFromDB("operations", function(err, res) {
+			if (err) {
+				callback(err, {error: 2});
+			} else {
+				response.json(res);
+				response.end();
+			}
+		});
+	}
+
+	if (dataInOperations.contract_id.length === 17 && dataInOperations.contract_id.startsWith("2625") && dataInOperations.bill !== 0) {
+		async.waterfall([
+			getUserCard,
+			changeUserBalance,
+			addNewOperation,
+			getAllDataAndResponce
+		],
+		function(error, errorCode) {
+			if (err) {
+				console.log(err);
+				response.json(errorCode);
+				response.end();
+			}
+		});
 	}
 });
+
+// Cancel last operation of user and change balance
 
 app.post('/api/cancel/', function(request, response) {
 	var data = request.body;
 
-	if (data.token === "weewquewiqy343ui12y43iughewriueyoqbewrioe") {
+	function getFromCards(callback) {
 		db.selectFromDBwithSelector("operations", data.contract_id, function(err, res) {
 			if (err) {
-				errorCallback(err, 3, response);
+				callback(err, {error: 8});
 			} else {
-				var lastAction = res[res.length - 1];
+				callback(null, res);
+			}
+		});
+	}
 
-				db.selectFromDBwithSelector("cards", lastAction.contract_id, function(err, res) {
-					if (err) {
-						errorCallback(err, 3, response);
-					} else {
-						var newBalance = res[0].balance + (lastAction.bill * (res[0].type === 'D' ? -1 : 1));
+	function getFromOperations(result, callback) {
+		var lastAction = result[result.length - 1];
 
-						db.changeRowInDB("cards", newBalance, lastAction.contract_id, function(err, res) {
-							if (err) {
-								errorCallback(err, 6, response);
-							}
-						});
-						db.removeFromDB("operations", lastAction.id, function(err, res) {
-							if (err) {
-								errorCallback(err, 6, response);
-							} else {
-								db.selectFromDB("operations", function(err, res) {
-									if (err) {
-										errorCallback(err, 6, response);
-									} else {
-										response.json(res);
-										response.end();
-									}
-								});
-							}
-						});
-					}
-				});
+		db.selectFromDBwithSelector("cards", lastAction.contract_id, function(err, res) {
+			if (err) {
+				callback(err, {error: 3});
+			} else {
+				callback(null, res, lastAction);
+			}
+		});
+	}
+
+	function changeInCards(result, lastAction, callback) {
+		var newBalance = result[0].balance + (lastAction.bill * (result[0].type === 'D' ? -1 : 1));
+
+		db.changeRowInDB("cards", newBalance, lastAction.contract_id, function(err, res) {
+			if (err) {
+				callback(err, {error: 6});
+			} else {
+				callback(null, lastAction);
+			}
+		});
+	}
+
+	function removeInOperations(lastAction, callback) {
+		db.removeFromDB("operations", lastAction.id, function(err, res) {
+			if (err) {
+				callback(err, {error: 9});
+			} else {
+				callback(null);
+			}
+		});
+	}
+
+	function getAllOperations(callback) {
+		db.selectFromDB("operations", function(err, res) {
+			if (err) {
+				callback(err, {error: 8});
+			} else {
+				response.json(res);
+				response.end();
+				callback(null);
+			}
+		});
+	}
+
+	if (data.token === "weewquewiqy343ui12y43iughewriueyoqbewrioe") {
+
+		// Start queue of cancel operation
+
+		async.waterfall([
+			getFromCards,
+			getFromOperations,
+			changeInCards,
+			removeInOperations,
+			getAllOperations
+		],
+		function(error, errorCode) {
+			if (error) {
+				console.log(error);
+				response.json(errorCode);
+				response.end();
 			}
 		});
 	} else {
